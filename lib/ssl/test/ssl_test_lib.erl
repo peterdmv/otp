36aa6a7978c4145f5ae077275e4f0d20e0382d27
Ingela Anderton Andin <ingela@erlang.org>
2019-09-23 16:14:24 +0200

    ssl: Set bigger OS buffers on Solaris

--- lib/ssl/test/ssl_test_lib.erl
+++ lib/ssl/test/ssl_test_lib.erl
@@ -2815,3 +2815,11 @@ skip_unstable_os() ->
         _ ->
             false
     end.
+
+socket_opts() ->
+    case os:type() of 
+        {unix,sunos} ->
+            [{recbuf, 2000000}, {sndbuf, 2000000}];
+        _ ->
+            []
+    end.
